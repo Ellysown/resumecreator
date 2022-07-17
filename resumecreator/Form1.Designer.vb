1d75d9e10d282492ca81848ab74789cb1323e7dc
Ellyson <xxLAxxjeje09@gmail.com>
2022-07-17 20:14:46 +0800

    Added Final Design Done

---
+++ resumecreator/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.convertbtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'convertbtn
+        '
+        Me.convertbtn.Location = New System.Drawing.Point(65, 77)
+        Me.convertbtn.Name = "convertbtn"
+        Me.convertbtn.Size = New System.Drawing.Size(139, 57)
+        Me.convertbtn.TabIndex = 0
+        Me.convertbtn.Text = "Convert to PDF"
+        Me.convertbtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(271, 213)
+        Me.Controls.Add(Me.convertbtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents convertbtn As Button
+End Class
